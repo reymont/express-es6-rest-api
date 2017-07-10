059713cc77dd0710188e4f526ba1e3e22520675d
李杨 <liyang@yihecloud.com>
2017-07-10 10:11:50 +0800

    添加 promise test

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,102 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
+import {
+	version
+} from '../../package.json';
+import {
+	Router
+} from 'express';
 import facets from './facets';
 
-export default ({ config, db }) => {
+export default ({
+	config,
+	db
+}) => {
 	let api = Router();
 
 	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	api.use('/elk', (req, res) => {
+const elasticsearch = require('elasticsearch');
+const esClient = new elasticsearch.Client({
+    host: '192.168.31.215:9200',
+    log: 'error'
+});
+
+const search = function search(index, body) {
+    return esClient.search({
+        index: index,
+        body: body
+    });
+};
+
+let body = {
+    "query": {
+        "bool": {
+            "filter": [{
+                "range": {
+                    "@timestamp": {
+                        "from": "1499483992610",
+                        "to": "1499583992610",
+                        "include_lower": true,
+                        "include_upper": true
+                    }
+                }
+            }]
+        }
+    },
+    "aggs": {
+        "methodCount": {
+            "terms": {
+                "field": "uri.keyword",
+                "size": 7,
+                "order": [{
+                        "_count": "desc"
+                    },
+                    {
+                        "_term": "asc"
+                    }
+                ]
+            },
+            "aggregations": {
+                "sum_request_time": {
+                    "sum": {
+                        "field": "request_time"
+                    }
+                }
+            }
+        }
+    }
+};
+
+search('logstash-*', body).then(results => {
+      console.log(`found ${results.hits.total} items in ${results.took}ms`);
+      if (results.hits.total > 0) console.log(`returned article titles:`);
+      results.hits.hits.forEach((hit, index) => console.log(`\t${++index} - ${hit._source.remote_addr} (score: ${hit._score})`));
+      console.log(JSON.stringify(results, null, 4));
+      console.log(`aggregations values.`);
+      
+      var jsonResult = new Array();
+      results.aggregations.methodCount.buckets.forEach((hit, index) => {
+            
+            console.log(`\t${++index} - ${hit.key} - ${hit.doc_count} `);
+            var jsonResultItem = new Object(); 
+            jsonResultItem.index = index
+            jsonResultItem.key = hit.key
+            jsonResultItem.doc_count = hit.doc_count
+            jsonResult[index] = jsonResultItem
+      });
+      console.log(new Date().getTime())
+	  res.json({
+			data:jsonResult
+		});
+    })
+    .catch(console.error);
+
+	});
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
-		res.json({ version });
+		res.json({
+			version
+		});
 	});
 
 	return api;
