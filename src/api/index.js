7d9865a1f735f69553de8bb27ee653336f498b40
李杨 <liyang@yihecloud.com>
2017-07-10 10:30:56 +0800

    按天显示最耗时的请求

--- src/api/index.js
+++ src/api/index.js
@@ -1,103 +1,126 @@
 import {
-	version
+    version
 } from '../../package.json';
 import {
-	Router
+    Router
 } from 'express';
 import facets from './facets';
 
 export default ({
-	config,
-	db
+    config,
+    db
 }) => {
-	let api = Router();
+    let api = Router();
 
-	// mount the facets resource
-	api.use('/elk', (req, res) => {
-const elasticsearch = require('elasticsearch');
-const esClient = new elasticsearch.Client({
-    host: '192.168.31.215:9200',
-    log: 'error'
-});
+    // mount the facets resource
+    api.use('/elk', (req, res) => {
+        const elasticsearch = require('elasticsearch');
+        const esClient = new elasticsearch.Client({
+            host: '192.168.31.215:9200',
+            log: 'error'
+        });
 
-const search = function search(index, body) {
-    return esClient.search({
-        index: index,
-        body: body
-    });
-};
+        const search = function search(index, body) {
+            return esClient.search({
+                index: index,
+                body: body
+            });
+        };
 
-let body = {
-    "query": {
-        "bool": {
-            "filter": [{
-                "range": {
-                    "@timestamp": {
-                        "from": "1499483992610",
-                        "to": "1499583992610",
-                        "include_lower": true,
-                        "include_upper": true
-                    }
+        let body = {
+            "query": {
+                "bool": {
+                    "filter": [{
+                        "range": {
+                            "@timestamp": {
+                                "from": "1499483992610",
+                                "to": "1499583992610",
+                                "include_lower": true,
+                                "include_upper": true
+                            }
+                        }
+                    }]
                 }
-            }]
-        }
-    },
-    "aggs": {
-        "methodCount": {
-            "terms": {
-                "field": "uri.keyword",
-                "size": 7,
-                "order": [{
-                        "_count": "desc"
-                    },
-                    {
-                        "_term": "asc"
-                    }
-                ]
             },
-            "aggregations": {
-                "sum_request_time": {
-                    "sum": {
-                        "field": "request_time"
+            "aggs": {
+                "result_agg": {
+                    "date_histogram": {
+                        "field": "@timestamp",
+                        "interval": "day",
+                        "min_doc_count": 0,
+                        "extended_bounds": {
+                            "min": "1499483992610",
+                            "max": "1499583992610"
+                        }
+                    },
+                    "aggs": {
+                        "methodCount": {
+                            "terms": {
+                                "field": "uri.keyword",
+                                "size": 7,
+                                "order": [{
+                                        "_count": "desc"
+                                    },
+                                    {
+                                        "_term": "asc"
+                                    }
+                                ]
+                            },
+                            "aggregations": {
+                                "sum_request_time": {
+                                    "sum": {
+                                        "field": "request_time"
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-};
+        };
 
-search('logstash-*', body).then(results => {
-      console.log(`found ${results.hits.total} items in ${results.took}ms`);
-      if (results.hits.total > 0) console.log(`returned article titles:`);
-      results.hits.hits.forEach((hit, index) => console.log(`\t${++index} - ${hit._source.remote_addr} (score: ${hit._score})`));
-      console.log(JSON.stringify(results, null, 4));
-      console.log(`aggregations values.`);
-      
-      var jsonResult = new Array();
-      results.aggregations.methodCount.buckets.forEach((hit, index) => {
-            
-            console.log(`\t${++index} - ${hit.key} - ${hit.doc_count} `);
-            var jsonResultItem = new Object(); 
-            jsonResultItem.index = index
-            jsonResultItem.key = hit.key
-            jsonResultItem.doc_count = hit.doc_count
-            jsonResult[index] = jsonResultItem
-      });
-      console.log(new Date().getTime())
-	  res.json({
-			data:jsonResult
-		});
-    })
-    .catch(console.error);
+        search('logstash-*', body).then(results => {
+                console.log(`found ${results.hits.total} items in ${results.took}ms`);
+                if (results.hits.total > 0) console.log(`returned article titles:`);
+                results.hits.hits.forEach((hit, index) => console.log(`\t${++index} - ${hit._source.remote_addr} (score: ${hit._score})`));
+                console.log(JSON.stringify(results, null, 4));
+                console.log(`aggregations values.`);
 
-	});
+                var jsonResult = new Array();
+                results.aggregations.result_agg.buckets.forEach((hit, index) => {
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({
-			version
-		});
-	});
+                    console.log(`\t${++index} - ${hit.key} - ${hit.doc_count} `);
+                    let jsonResultItem = new Object();
+                    //jsonResultItem.index = index
+                    jsonResultItem.key = hit.key
+                    jsonResultItem.doc_count = hit.doc_count
+                    jsonResultItem.data = new Array();
 
-	return api;
+                    hit.methodCount.buckets.forEach((hit, index)=>{
+                        let request = new Object();
+                        request.key = hit.key
+                        request.doc_count = hit.doc_count
+                        request.value = hit.sum_request_time.value
+                        jsonResultItem.data[index] = request;
+                    });
+
+                    jsonResult[index] = jsonResultItem
+                });
+                console.log(new Date().getTime())
+                res.json({
+                    data: jsonResult
+                });
+            })
+            .catch(console.error);
+
+    });
+
+    // perhaps expose some API metadata at the root
+    api.get('/', (req, res) => {
+        res.json({
+            version
+        });
+    });
+
+    return api;
 }
